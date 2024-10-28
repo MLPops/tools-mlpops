0725fd93634826cc88e7b3e3179531d37dc2a339
Artemiy Mazaew <a.mazaew@lambda-it.ru>
2024-10-29 01:39:46 +0300

    Поправил сборку

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://tools.mlpops.ru/',
   outDir: './build',
-  output: "server",
-  adapter: netlify()
+  integrations: [react()],
 });
